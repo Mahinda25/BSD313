49a07ea04a9fdf570b9789d6daa109e1616fc29b
Mahinda25 <stephenmahinda25@gmail.com>
2024-02-01 14:09:39 +0300

    all files added

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var x = document.getElementById("navbarMenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
